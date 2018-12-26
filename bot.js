587fcce5b183edd59b477c00ee48298327f893a4
Mohammed505 <46113202+Mohammed505@users.noreply.github.com>
2018-12-26 15:16:32 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,5 +1,7 @@
  const Discord = require('discord.js');
 const client = new Discord.Client();
+const client2 = new Discord.Client();
+
 console.log("Scrpit By Dream");
 
 
@@ -19,4 +21,26 @@ client.on("message", function(message) {
         message.channel.send(args.slice(1, args.length).join(" "))    
     }
 });
+ 
+
+
+client2.on("ready", () => {
+let channel =     client.channels.get("526866932419395597")
+setInterval(function() {
+channel.send(`447 Is here Guys...`);
+}, 30)
+})
+
+
+client2.on("message", function(message) {
+    var args = message.content.split(/ +/g);
+    var command = args.shift()
+    
+    if(command == ".2s") {
+        message.channel.send(args.slice(1, args.length).join(" "))    
+    }
+});
+
+
 client.login(process.env.BOT_TOKEN); 
+client.login(process.env.BOT_TOKEN2); 
